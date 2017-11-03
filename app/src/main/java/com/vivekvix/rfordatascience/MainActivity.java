8c25e3397be254ec318cc1799a1fa7e1a6b0cba4
vivekvix <vivekvix@users.noreply.github.com>
2017-11-03 19:26:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/vivekvix/rfordatascience/MainActivity.java
+++ app/src/main/java/com/vivekvix/rfordatascience/MainActivity.java
@@ -19,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         Chapter1 = (Button)findViewById(R.id.tips1);
         Chapter1.setOnClickListener(new OnClickListener() {
 
+       // get our html content
+        String htmlAsString = getString(R.string.html);      // used by WebView
+        //Spanned htmlAsSpanned = Html.fromHtml(htmlAsString); // used by TextView
+
+        // set the html content on a TextView
+       // TextView textView = (TextView) findViewById(R.id.textView);
+      //  textView.setText(htmlAsSpanned);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
+            
+            
+            
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
