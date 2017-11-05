a51c334943ad4733c569e9a6d35f4ab634a740c7
vivekvix <vivekvix@gmail.vom>
2017-11-05 20:44:05 +0530

    1st upload

--- app/src/main/java/com/vivekvix/rfordatascience/MainActivity.java
+++ app/src/main/java/com/vivekvix/rfordatascience/MainActivity.java
@@ -9,9 +9,10 @@ import android.view.View.OnClickListener;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    Button Chapter1, Chapter2, tips3, tips4, tips5;
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    Button Chapter1, Chapter2, Chapter3, tips4, tips5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,33 +20,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Chapter1 = (Button)findViewById(R.id.tips1);
-        Chapter1.setOnClickListener(new OnClickListener() {
-
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                Intent i = new Intent(MainActivity.this, chapter1.class);
-                startActivity(i);
-
-            }
-        });
-
-
         Chapter2 = (Button)findViewById(R.id.tips2);
-          Chapter2.setOnClickListener(new OnClickListener() {
+        Chapter3 = (Button)findViewById(R.id.tips3);
+        Chapter1.setOnClickListener(this);
+        Chapter2.setOnClickListener(this);
+        Chapter3.setOnClickListener(this);
 
-        @Override
-        public void onClick(View arg0) {
+
+     //   Chapter1 = (Button)findViewById(R.id.tips1);
+    //    Chapter1.setOnClickListener(new OnClickListener() {
+    //        @Override
+    //        public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+    //            Intent i = new Intent(MainActivity.this, chapter1.class);
+    //            startActivity(i);
+    //        }
+    //    });
+
+
+    //    Chapter2 = (Button)findViewById(R.id.tips2);
+        //     Chapter2.setOnClickListener(new OnClickListener() {
+
+    //    @Override
+    //    public void onClick(View arg0) {
             // TODO Auto-generated method stub
-            Intent i = new Intent(MainActivity.this, chapter2.class);
-            startActivity(i);
+    //        Intent i = new Intent(MainActivity.this, chapter2.class);
+    //        startActivity(i);
 
-        }
-        });
+    //    }
+    //    });
 
         // get our html content
 
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch(v.getId())
+        {
+            case R.id.tips1 :
+                Toast.makeText(MainActivity.this,"tips1", 1000).show();
+                String htmlAsString = getString(R.string.html);
+                Intent i = new Intent(MainActivity.this, chapter1.class);
+                i.putExtra("keyName",htmlAsString);
+                startActivity(i);
+                break;
+            case R.id.tips2 :
+                Toast.makeText(MainActivity.this,"tips2", 1000).show();
+                String htmlAsString1 = getString(R.string.html1);
+                Intent i1 = new Intent(MainActivity.this, chapter1.class);
+                i1.putExtra("keyName",htmlAsString1);
+                startActivity(i1);
+                break;
+            case R.id.tips3 :
+                Toast.makeText(MainActivity.this,"tips3", 1000).show();
+                break;
+
+
+        }
     }
 }
--- app/src/main/java/com/vivekvix/rfordatascience/chapter1.java
+++ app/src/main/java/com/vivekvix/rfordatascience/chapter1.java
@@ -3,6 +3,8 @@ package com.vivekvix.rfordatascience;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 public class chapter1 extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class chapter1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter1);
+        String data = getIntent().getExtras().getString("keyName");
+     //   String htmlAsString = getString(R.string.data);
+        WebView webView = (WebView) findViewById(R.id.textView1);
+        //     webView.setWebViewClient(new MyBrowser());
+        //webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadDataWithBaseURL("",data,"text/html", "utf-8", "");
+      //  final TextView mTextView = (TextView) findViewById(R.id.textView1);
+      //  mTextView.setText(data);
     }
 
 
